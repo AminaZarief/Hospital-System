9345ca5afeb6c2cc6bbf09c88fe637ab4ace2d48
AminaZarief <aminaabukaied@gmail.com>
2026-08-13 01:27:25 +0300

    update design of patient lst

--- hospital_sys.py
+++ hospital_sys.py
@@ -9,6 +9,10 @@ def get_valid_input(prompt, min_val, max_val):
         except ValueError:
             print('please enter a valid number')
 
+
+NUMBER_OF_SPECIALIZATIONS = 20
+STATUS = ['Normal', 'Urgent', 'Super Urgent']
+
 class Patient: 
     def __init__(self, name, status, specialization):
         self.name = name
@@ -16,18 +20,18 @@ class Patient:
         self.specialization = specialization 
 
     def __repr__(self):
-        return f'Patient: {self.name} is {self.status}'
+        return f'Patient: {self.name} is {STATUS[self.status]}'
         
 
 
 
 class Hospital:
     def __init__(self):
-        self.patients_lst = [] 
+        self.patients_lst = [ [] for i in range(NUMBER_OF_SPECIALIZATIONS)] 
 
     def add_patient(self):
         # Step1 get patient data
-        specialization = get_valid_input('Enter specialization ', 1, 20)    
+        specialization = get_valid_input('Enter specialization ', 1, NUMBER_OF_SPECIALIZATIONS)    
         name = input('Enter patient name: ')                 
         status = get_valid_input('Enter status(0 normal / 1 urgent / 2 super urgent: )', 0, 2)
         
@@ -35,19 +39,41 @@ class Hospital:
         patient = Patient(name, status, specialization)
 
         # step3 add patient 
-        self.patients_lst.append(patient)
+        self.patients_lst[specialization-1].append(patient)
 
     def print_all_patients(self):
-        for patient in self.patients_lst:
-           print(patient)
+        
+        
+        for idx, patients in enumerate(self.patients_lst):
+           if len(patients) > 0:
+               print(f'Specialization {idx + 1}: There are {len(patients)} patients.')
+               for patient in patients:
+                    print(patient)
+
+    def get_next_patient(self):...
 
 
 if __name__ == '__main__':
     hospital = Hospital()
-    print(len(hospital.patients_lst))
-    hospital.add_patient()
-    print(len(hospital.patients_lst))
-    hospital.print_all_patients()
+    while True:
+        print('Program Options:')
+        print('1) Add new patients')
+        print('2) print all patients')
+        print('3) Get next patient')
+        print('4) Remove a leaving patient')
+        print('5) End the progeam')
+
+        choice = int(input('Enter your choice(from 1 to 5): '))
+        if choice == 1:
+            hospital.add_patient()
+        elif choice == 2:
+            hospital.print_all_patients()
+        elif choice == 3: 
+            hospital.get_next_patient()
+        elif choice == 5:
+            break
+        else:
+            print('Not valid number, please enter number from 1 to 5')
 
 
 
